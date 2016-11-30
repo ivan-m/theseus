c186b0888d894b558ee1855308a70d4317866f92
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2016-11-30 17:00:50 +1100

    Start work on a test suite

---
+++ test/propTests.hs
@@ -0,0 +1,16 @@
+{- |
+   Module      : Main
+   Description : Property-based tests
+   Copyright   : Ivan Lazar Miljenovic, Patryk Zadarnowski
+   License     : MIT
+   Maintainer  : Ivan.Miljenovic@gmail.com
+
+
+
+ -}
+module Main where
+
+--------------------------------------------------------------------------------
+
+main :: IO ()
+main = putStrLn "No tests yet!"
