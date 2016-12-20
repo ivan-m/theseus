bd30354b2b5dc4612f6c0118cf67643b690efd2c
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2016-12-20 15:25:50 +1100

    Wibble

--- test/propTests.hs
+++ test/propTests.hs
@@ -76,7 +76,7 @@ encodeDecode _ a = either (const False) (a==) (unravel (ravel a))
 --------------------------------------------------------------------------------
 
 data LotsOfConstructors = C0 | C1 | C2 | C3 | C4 | C5 | C6 | C7 | C8 | C9
-  deriving (Eq, Show, Bounded, Enum, Generic,Theseus)
+  deriving (Eq, Show, Bounded, Enum, Generic, Theseus)
 
 instance Arbitrary LotsOfConstructors where
   arbitrary = arbitraryBoundedEnum
